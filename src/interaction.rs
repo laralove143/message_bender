9983d63f17551a9c08e770d33120c7930700e960
laralove143 <laralove143@icloud.com>
2022-04-09 00:25:11 +0300

    add boilerplate

---
+++ src/interaction.rs
@@ -0,0 +1,37 @@
+use anyhow::{bail, Result};
+use twilight_model::{
+    application::interaction::Interaction,
+    http::interaction::{InteractionResponse, InteractionResponseType},
+};
+
+use crate::Context;
+
+impl Context {
+    pub async fn handle_interaction(&self, interaction: Interaction) -> Result<()> {
+        let client = self.http.interaction(self.application_id);
+
+        let command = if let Interaction::ApplicationCommand(cmd) = interaction {
+            *cmd
+        } else {
+            bail!("unknown interaction: {interaction:#?}");
+        };
+
+        let response_data = match command.data.name.as_str() {
+            _ => bail!("unknown command: {command:#?}"),
+        };
+
+        client
+            .create_response(
+                command.id,
+                &command.token,
+                &InteractionResponse {
+                    kind: InteractionResponseType::ChannelMessageWithSource,
+                    data: Some(response_data),
+                },
+            )
+            .exec()
+            .await?;
+
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,139 @@
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::blanket_clippy_restriction_lints,
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::multiple_inherent_impl,
+    clippy::missing_errors_doc
+)]
+
+mod interaction;
+
+use std::{env, ops::Deref, sync::Arc};
+
+use anyhow::Result;
+use futures_util::StreamExt;
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::{Cluster, EventTypeFlags};
+use twilight_http::Client;
+use twilight_model::{
+    gateway::{event::Event, Intents},
+    id::{
+        marker::{ApplicationMarker, GuildMarker},
+        Id,
+    },
+};
+
+pub struct ContextInner {
+    http: Client,
+    cache: InMemoryCache,
+    application_id: Id<ApplicationMarker>,
+}
+
+pub struct Context(Arc<ContextInner>);
+
+impl Clone for Context {
+    fn clone(&self) -> Self {
+        Self(Arc::clone(&self.0))
+    }
+}
+
+impl Deref for Context {
+    type Target = Arc<ContextInner>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Context {
+    #[allow(clippy::print_stderr, clippy::wildcard_enum_match_arm)]
+    async fn handle_event(self, event: Event) {
+        if let Err(err) = match event {
+            Event::InteractionCreate(interaction) => self.handle_interaction(interaction.0).await,
+            _ => Ok(()),
+        } {
+            eprintln!("{err}");
+            if let Err(e) = self.inform_error().await {
+                eprintln!("error when informing owner: {e}");
+            }
+        }
+    }
+
+    async fn inform_error(&self) -> Result<()> {
+        self.http
+            .create_message(
+                self.http
+                    .create_private_channel(
+                        self.http.current_user().exec().await?.model().await?.id,
+                    )
+                    .exec()
+                    .await?
+                    .model()
+                    .await?
+                    .id,
+            )
+            .content("an error occurred :(")?
+            .exec()
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let intents = Intents::empty();
+    let event_types = EventTypeFlags::INTERACTION_CREATE;
+    let resource_types = ResourceType::empty();
+
+    let test_guild_id: Option<Id<GuildMarker>> = env::var("TEST_GUILD_ID")
+        .ok()
+        .and_then(|id| id.parse().ok())
+        .map(Id::new);
+
+    let token = env::var(if test_guild_id.is_some() {
+        "TEST_BOT_TOKEN"
+    } else {
+        "EDIT_BOT_TOKEN"
+    })?;
+
+    let (cluster, mut events) = Cluster::builder(token.clone(), intents)
+        .event_types(event_types)
+        .build()
+        .await?;
+    let cluster_spawn = Arc::new(cluster);
+    tokio::spawn(async move { cluster_spawn.up().await });
+
+    let http = Client::new(token);
+
+    let application_id = http
+        .current_user_application()
+        .exec()
+        .await?
+        .model()
+        .await?
+        .id;
+
+    http.interaction(application_id)
+        .set_global_commands(&[])
+        .exec()
+        .await?;
+
+    let cache = InMemoryCache::builder()
+        .resource_types(resource_types)
+        .build();
+
+    let ctx = Context(Arc::new(ContextInner {
+        http,
+        cache,
+        application_id,
+    }));
+
+    while let Some((_, event)) = events.next().await {
+        ctx.cache.update(&event);
+        tokio::spawn(ctx.clone().handle_event(event));
+    }
+
+    Ok(())
+}
